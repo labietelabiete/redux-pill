54784de37b4ace4df7f798c51094ca48fa523e2e
labietelabiete <victorhugogote@gmail.com>
2021-09-09 10:14:02 +0200

    first drawing home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,26 @@ import React from "react";
 
 import SearchBar from "../../components/SearchBar";
 
+import "./Home.scss";
+
+import logo from "../../assets/images/logo.png";
+
 export default function Home() {
-  return <SearchBar alignment="text-center" />;
+  return (
+    <div className="home-container container mt-5">
+      <div className="row">
+        <div className="col text-center">
+          <img className="logo" src={logo} alt="" />
+        </div>
+      </div>
+      <div className="row text-center">
+        {/* <div className="col-12 text-center">
+          <h3 className="colorGrey">Stop being an Okupa!</h3>
+        </div> */}
+        <div className="col-12 text-center">
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
 }
